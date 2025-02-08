79856ae1b8ee12fe798ec1a021df8154837948ee
NangzaiWebDeveloper <abujamnangzai00@gmail.com>
2025-02-09 02:03:18 +0600

    this is count down timmer

--- src/components/Seven.jsx
+++ src/components/Seven.jsx
@@ -1,19 +1,19 @@
 import { useEffect, useState } from "react";
 
-
 const Seven = () => {
 
     const [time, setTime] = useState(60)
 
     useEffect(()=>{
-        const timer = setTimeout(()=>setTime(time-1), 1000)
-        return ()=> clearTimeout(timer)
-    },[time])
+        if(time>0){
+            const timer = setTimeout(()=> setTime(time-1),1000)
+            return ()=>clearTimeout(timer)
+        }
+    }, [time])
 
     return (
-        //Build a timer that counts down from a specified time.
         <div>
-            <h4>Time left {time} second</h4>
+            Time Left : {time} second
         </div>
     );
 };
